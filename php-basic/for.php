62c2e11e558c2df2066bbeec169833c58c38cd05
nao-kichi <namnam322@gmail.com>
2022-09-05 01:16:17 +0900

    change

---
+++ php-basic/for.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         // 1～10までの整数を順番に出力する
+         for ($i = 1; $i <= 10; $i++) {
+             echo $i . '<br>';
+         }
+         ?>
+     </p>
+ </body>
+ 
+ </html>
